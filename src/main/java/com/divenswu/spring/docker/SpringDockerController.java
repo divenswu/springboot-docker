2e1aabfd0f9dc4f02405470489be68511dff84cf
divenwu <divenwu@kuainiugroup.com>
2020-08-24 18:48:22 +0800

    init

--- src/main/java/com/divenswu/spring/docker/SpringDockerController.java
+++ src/main/java/com/divenswu/spring/docker/SpringDockerController.java
@@ -3,6 +3,7 @@ package com.divenswu.spring.docker;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -22,6 +23,8 @@ public class SpringDockerController {
     @RequestMapping(value = "/**", method = RequestMethod.GET)
     @ResponseBody
     private Map<String, Object> home(HttpServletRequest request) {
+        //处理任务
+        long costTime = this.doWork(request);
         // 获取服务主机信息
         String ip = "unknown";
         String hostName = "unknown";
@@ -46,8 +49,39 @@ public class SpringDockerController {
         map.put("hostIp", ip);
         map.put("hostName", hostName);
         map.put("requestPath", requestPath);
+        map.put("costTime", costTime);
         logger.info("get request for : "+ requestPath);
         return map;
+    }
+
+    private long doWork(HttpServletRequest request){
+        long a = System.currentTimeMillis();
+        String timeoutStr = request.getParameter("timeout");
+        if(!StringUtils.isEmpty(timeoutStr)){
+            int timeout = 0;
+            try {
+                timeout = Integer.parseInt(timeoutStr);
+            } catch (Exception e){}
+            if (timeout > 0){
+                long runNum = 0L;
+                for(int i=0; i< 1000000000L * timeout; i++){
+                    runNum += i;
+                }
+                System.out.println("runNum="+runNum);
+            }
+        }
+        long b = System.currentTimeMillis();
+        return (b-a);
+    }
+
+    public static void main(String[] args) {
+        long a = System.currentTimeMillis();
+        long num = 0;
+        for(int i=0; i< 1000000000; i++){
+            num += i;
+        }
+        long b = System.currentTimeMillis();
+        System.out.println(b-a);
     }
 
 }
